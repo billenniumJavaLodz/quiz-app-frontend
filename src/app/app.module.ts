19025bc9eddc7e1ad1c0a7c48978aafa92296ed7
Szymon <38376863+beatshaker@users.noreply.github.com>
2020-04-08 13:24:10 +0200

    -simple page not found, add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { MaterialModule } from './modules/material/material.module'
-import { httpInterceptorProviders } from './interceptors/http-interceptor-providers';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/header/header.component';
+import {MaterialModule} from './modules/material/material.module'
+import {httpInterceptorProviders} from './interceptors/http-interceptor-providers';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomePageComponent
+    HomePageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
