1a50291b560acc82cbadcc1c905a07643a9cef76
Szymon <38376863+beatshaker@users.noreply.github.com>
2020-04-08 13:24:10 +0200

    -simple page not found, add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'user/:id',
     component: HomePageComponent,
+    pathMatch: 'full',
+
+  }, {
+    path: '**',
+    component: PageNotFoundComponent,
     pathMatch: 'full'
   }
-  //toDo PageNotFound
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
